5466e9bb46dbd422cc89e868cb281fa621eba2ac
William Correa <wilcorrea@gmail.com>
2026-02-21 22:42:15 -0300

    chore(homebrew): update Cask to v0.2.1 with real SHA256 hashes

--- homebrew/Casks/arandu.rb
+++ homebrew/Casks/arandu.rb
@@ -1,12 +1,12 @@
 cask "arandu" do
-  version "0.2.0"
+  version "0.2.1"
 
   on_arm do
-    sha256 "PLACEHOLDER_ARM_SHA256"
+    sha256 "e44338b83a0c55b2aa66c2e0ecc31a585a3f2fc216af026a5f46aca03d40a69e"
     url "https://github.com/devitools/arandu/releases/download/v#{version}/Arandu_#{version}_aarch64.dmg"
   end
   on_intel do
-    sha256 "PLACEHOLDER_INTEL_SHA256"
+    sha256 "77f9c63b054377ee0e4648646356eeeea36128816c52b3d8d1cfeb38d003bb4c"
     url "https://github.com/devitools/arandu/releases/download/v#{version}/Arandu_#{version}_x64.dmg"
   end
 
